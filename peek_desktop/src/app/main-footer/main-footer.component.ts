432f52be120a8206d905cccb77f903b4e836d048
Jarrod Chesney <jarrod.chesney@synerty.com>
2017-09-21 08:56:43 +1000

    Updated to peek-util 0.5.3

--- peek_desktop/src/app/main-footer/main-footer.component.ts
+++ peek_desktop/src/app/main-footer/main-footer.component.ts
@@ -1,5 +1,5 @@
 import {Component} from "@angular/core";
-import {ConfigLink, FooterService, NavBackService} from "@synerty/peek-util";
+import {ConfigLink, FooterService, NavBackService, TitleService} from "@synerty/peek-util";
 import {ComponentLifecycleEventEmitter, VortexStatusService} from "@synerty/vortexjs";
 
 @Component({
@@ -16,7 +16,8 @@ export class MainFooterComponent extends ComponentLifecycleEventEmitter {
     isEnabled: boolean = true;
 
     constructor(vortexStatusService: VortexStatusService,
-                footerService: FooterService) {
+                footerService: FooterService,
+                titleService: TitleService) {
         super();
 
         this.configLinks = footerService.configLinksSnapshot;
@@ -25,7 +26,7 @@ export class MainFooterComponent extends ComponentLifecycleEventEmitter {
             .takeUntil(this.onDestroyEvent)
             .subscribe(v => this.statusText = v);
 
-        footerService.isEnabled
+        titleService.isEnabled
             .takeUntil(this.onDestroyEvent)
             .subscribe(v => this.isEnabled = v);
 
