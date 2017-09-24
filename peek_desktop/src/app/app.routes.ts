0fd9a0051d8e956d432e46811d6350743c466035
Jarrod Chesney <jarrod.chesney@synerty.com>
2017-09-24 20:32:30 +1000

    routes now use enrollment guard

--- peek_desktop/src/app/app.routes.ts
+++ peek_desktop/src/app/app.routes.ts
@@ -1,16 +1,28 @@
 import {MainHomeComponent} from "./main-home/main-home.component";
 import {UnknownRouteComponent} from "./unknown-route/unknown-route.component";
 import {pluginRoutes} from "../plugin-routes";
+
+import {DeviceEnrolledGuard} from "@peek/peek_core_device";
+
 export const staticRoutes = [
     {
-        path: "",
-        component: MainHomeComponent,
-        data: {title: "Home"}
+        path: 'peek_core_device',
+        loadChildren: "peek_core_device/device.module#DeviceModule"
     },
-    ...pluginRoutes,
+    // All routes require the device to be enrolled
+    {
+        path: '',
+        canActivate: [DeviceEnrolledGuard],
+        children: [
+            {
+                path: '',
+                component: MainHomeComponent
+            },
+            ...pluginRoutes
+        ]
+    },
     {
         path: "**",
-        component: UnknownRouteComponent,
-        data: {title: "Route Error"}
+        component: UnknownRouteComponent
     }
 ];
