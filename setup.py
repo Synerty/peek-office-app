0a63067b8ca1cfef31db46a55a510ff1a39cad3c
Jarrod Chesney <jarrod.chesney@synerty.com>
2018-01-05 10:06:03 +1000

    Updated to version 0.7.9.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 pip_package_name = "peek-desktop"
 py_package_name = "peek_desktop"
 
-package_version = '0.7.9'
+package_version = '0.7.9.3'
 
 egg_info = "%s.egg-info" % pip_package_name
 if os.path.isdir(egg_info):
