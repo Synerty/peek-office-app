80ea94799b9c966815650da83193290ca0730650
Jarrod Chesney <jarrod.chesney@synerty.com>
2018-04-07 09:41:58 +1000

    Fixed setup.py not having a valid version number
    ORMOB-544

--- publish.sh
+++ publish.sh
@@ -25,31 +25,46 @@ fi
 
 echo "Setting version to $VER"
 
-VER_FILES="setup.py"
-VER_FILES="${VER_FILES} ${PACKAGE}/__init__.py"
-VER_FILES="${VER_FILES} peek_desktop/src/app/main-config/main-config.component.dweb.html"
+# Update the setup.py
+sed -i "s;^package_version.*=.*;package_version = '${VER}';"  setup.py
 
-for file in ${VER_FILES}
-do
-    sed -i "s/###PEEKVER###/${VER}/g" $file
-done
+function updateFileVers {
+    VER_FILES=""
+    VER_FILES="${VER_FILES} ${PACKAGE}/__init__.py"
+    VER_FILES="${VER_FILES} peek_desktop/src/app/main-config/main-config.component.dweb.html"
+
+    for file in ${VER_FILES}
+    do
+        sed -i "s/###PEEKVER###/${VER}/g" $file
+    done
+}
 
 
 # Upload to test pypi
 if [[ ${VER} == *"dev"* ]]; then
+    updateFileVers
     python setup.py  sdist --format=gztar
     git reset --hard
 
 else
+    # Commit the version number to setup.py
+    # This is needed for setup develop
+    git commit -a -m "Updated to version ${VER}"
+
+    # Apply the version to the other files
+    updateFileVers
+
+    # Create the package and upload to pypi
     python setup.py sdist --format=gztar upload
+
+    # Reset all the other versions, except setup.py
     git reset --hard
 
+    # Tag the release
     git tag ${VER}
     git push
     git push --tags
 fi
-
-
 
 
 echo "Publish Complete"
