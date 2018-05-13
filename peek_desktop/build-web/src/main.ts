99762bf5d731f45b72dfb0463378d5ccf4db52a2
Jarrod Chesney <jarrod.chesney@synerty.com>
2018-05-13 21:40:47 +1000

    Added support for VortexJS client name

--- peek_desktop/build-web/src/main.ts
+++ peek_desktop/build-web/src/main.ts
@@ -4,6 +4,9 @@ import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
 import {enableProdMode} from "@angular/core";
 import {environment} from "environments/environment";
 
+import {VortexService} from "@synerty/vortexjs";
+VortexService.setVortexClientName("peek-desktop");
+
 if (environment.production) {
     enableProdMode();
 }
