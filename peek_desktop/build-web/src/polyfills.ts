13133e42be5d96c404e1dbe01932d4f9b9efd13f
Jarrod Chesney <jarrod.chesney@synerty.com>
2018-06-10 00:55:25 +1000

    Added 'rxjs/add/operator/toPromise' to polyfills

--- peek_desktop/build-web/src/polyfills.ts
+++ peek_desktop/build-web/src/polyfills.ts
@@ -63,6 +63,7 @@ import 'rxjs/add/operator/zip';
 import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/takeUntil';
 import 'rxjs/add/operator/first';
+import 'rxjs/add/operator/toPromise';
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
