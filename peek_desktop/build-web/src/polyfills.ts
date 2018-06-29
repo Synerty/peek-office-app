1a211313b5037c1311482f97877404f6a95e9b50
Jarrod Chesney <jarrod.chesney@synerty.com>
2018-06-29 22:00:55 +1000

    Added more rxjs polyfills

--- peek_desktop/build-web/src/polyfills.ts
+++ peek_desktop/build-web/src/polyfills.ts
@@ -58,12 +58,14 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 
 
 import 'rxjs/add/observable/zip';
+import 'rxjs/add/observable/interval';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/zip';
 import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/takeUntil';
 import 'rxjs/add/operator/first';
 import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/debounce';
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
