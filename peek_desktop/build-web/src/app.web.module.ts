de76ad7a51476bb4766f68efbe51a934a866c3a2
Jacob Sorrell <jacob.sorrell@synerty.com>
2020-08-21 16:51:37 +1000

    PEEK-639: Upgrade Angular from 8.X to 9.1

--- peek_desktop/build-web/src/app.web.module.ts
+++ peek_desktop/build-web/src/app.web.module.ts
@@ -1,83 +1,85 @@
-// Angular
-import {BrowserModule} from "@angular/platform-browser";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-
-import {NgModule} from "@angular/core";
-import {RouterModule} from "@angular/router";
-// @synerty
-import {Ng2BalloonMsgModule} from "@synerty/ng2-balloon-msg-web";
-import {PeekModuleFactory} from "@synerty/peek-util-web";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { NzIconModule } from "ng-zorro-antd/icon";
+import { RouterModule } from "@angular/router";
+import { HttpClientModule } from "@angular/common/http";
+import { Ng2BalloonMsgModule } from "@synerty/ng2-balloon-msg-web";
 import {
     TupleActionPushOfflineSingletonService,
     TupleDataObservableNameService,
     TupleOfflineStorageNameService,
     TupleStorageFactoryService,
-    WebSqlFactoryService
+    WebSqlFactoryService,
 } from "@synerty/vortexjs";
-
 import {
     TupleStorageFactoryServiceWeb,
-    WebSqlBrowserFactoryService
+    WebSqlBrowserFactoryService,
 } from "@synerty/vortexjs/index-browser";
-// Routes
-import {staticRoutes} from "./app/app.routes";
-import {peekRootServices} from "./app/app.services";
-// This app
-import {AppComponent} from "./app/app.component";
-import {MainHomeComponent} from "./app/main-home/main-home.component";
-import {MainConfigComponent} from "./app/main-config/main-config.component";
-import {MainSidebarComponent} from "./app/main-sidebar/main-sidebar.component";
-import {UnknownRouteComponent} from "./app/unknown-route/unknown-route.component";
-import {pluginRootModules} from "./plugin-root-modules";
-import {pluginRootServices} from "./plugin-root-services";
-import {PluginRootComponent} from "./app/plugin-root.component";
-import {en_US, NgZorroAntdModule, NZ_I18N} from 'ng-zorro-antd';
-/** config angular i18n **/
-import {registerLocaleData} from '@angular/common';
-import en from '@angular/common/locales/en';
-import {SearchModule} from "peek_core_search/search.module";
+import { staticRoutes } from "./app/app.routes";
+import { peekRootServices } from "./app/app.services";
+import { AppComponent } from "./app/app.component";
+import { MainHomeComponent } from "./app/main-home/main-home.component";
+import { MainConfigComponent } from "./app/main-config/main-config.component";
+import { MainSidebarComponent } from "./app/main-sidebar/main-sidebar.component";
+import { UnknownRouteComponent } from "./app/unknown-route/unknown-route.component";
+import { pluginRootModules } from "./plugin-root-modules";
+import { pluginRootServices } from "./plugin-root-services";
+import { PluginRootComponent } from "./app/plugin-root.component";
+import { en_US, NgZorroAntdModule, NZ_I18N } from "ng-zorro-antd";
+import { registerLocaleData } from "@angular/common";
+import en from "@angular/common/locales/en";
+import { SearchModule } from "peek_core_search/search.module";
 
 registerLocaleData(en);
 
 export function tupleDataObservableNameServiceFactory() {
-    return new TupleDataObservableNameService("peek_client", {"plugin": "peek_client"});
+    return new TupleDataObservableNameService("peek_client", {
+        plugin: "peek_client",
+    });
 }
 
 export function tupleOfflineStorageNameServiceFactory() {
     return new TupleOfflineStorageNameService("peek_client");
 }
 
-
 @NgModule({
-    declarations: [AppComponent,
+    declarations: [
+        AppComponent,
         MainHomeComponent,
         MainConfigComponent,
         MainSidebarComponent,
         UnknownRouteComponent,
-        PluginRootComponent],
+        PluginRootComponent,
+    ],
     bootstrap: [AppComponent],
     imports: [
-        RouterModule,
-        PeekModuleFactory.RouterModule.forRoot(staticRoutes),
+        RouterModule.forRoot(staticRoutes),
         BrowserModule,
         BrowserAnimationsModule,
-        ...PeekModuleFactory.FormsModules,
+        HttpClientModule,
+        FormsModule,
+        NzIconModule,
         Ng2BalloonMsgModule,
         ...pluginRootModules,
         NgZorroAntdModule,
-        SearchModule
+        SearchModule,
     ],
     providers: [
-        {provide: NZ_I18N, useValue: en_US},
-        {provide: WebSqlFactoryService, useClass: WebSqlBrowserFactoryService},
-        {provide: TupleStorageFactoryService, useClass: TupleStorageFactoryServiceWeb},
+        { provide: NZ_I18N, useValue: en_US },
+        {
+            provide: WebSqlFactoryService,
+            useClass: WebSqlBrowserFactoryService,
+        },
+        {
+            provide: TupleStorageFactoryService,
+            useClass: TupleStorageFactoryServiceWeb,
+        },
         TupleActionPushOfflineSingletonService,
-
 
         ...peekRootServices,
         ...pluginRootServices,
-    ]
+    ],
 })
-export class AppWebModule {
-
-}
+export class AppWebModule {}
--- peek_desktop/build-web/src/polyfills.ts
+++ peek_desktop/build-web/src/polyfills.ts
@@ -41,8 +41,9 @@
 
 
 /** Evergreen browsers require these. **/
-import 'core-js/es6/reflect';
-import 'core-js/es7/reflect';
+
+import 'core-js/es/reflect';
+(window as any).global = window;
 
 
 /** ALL Firefox browsers require the following to support `@angular/animation`. **/
