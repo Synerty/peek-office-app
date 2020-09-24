f43e56d32809fbbf4f583c4d3067f6648faa453b
Jacob Sorrell <jacob.sorrell@synerty.com>
2020-09-24 10:47:33 +1000

    PEEK-674 Updated npm dependencies and fixed errors

--- peek_desktop/src/main.ts
+++ peek_desktop/src/main.ts
@@ -2,9 +2,9 @@ import { platformBrowserDynamic } from "@angular/platform-browser-dynamic"
 import { enableProdMode } from "@angular/core"
 import { environment } from "./environments/environment"
 import { VortexService } from "@synerty/vortexjs"
-import { AppModule } from "./app.module"
+import { AppModule } from "./app/app.module"
 
-const protocol = location.protocol.toLowerCase() == "https:" ? "wss" : "ws"
+const protocol = location.protocol.toLowerCase() === "https:" ? "wss" : "ws"
 
 VortexService.setVortexUrl(`${protocol}://${location.hostname}:${location.port}/vortexws`)
 VortexService.setVortexClientName("peek-desktop")
