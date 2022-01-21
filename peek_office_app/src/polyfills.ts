c502905228caaec5fb1ecf78b5af591635fc56d0
Jarrod Chesney <jarrod.chesney@synerty.com>
2022-01-21 20:04:17 +1000

    PEEK-1382 Removed rxjs-compat

--- peek_office_app/src/polyfills.ts
+++ peek_office_app/src/polyfills.ts
@@ -49,15 +49,6 @@
  * Zone JS is required by Angular itself.
  */
 import "zone.js/dist/zone"; // Included with Angular CLI.
-import "rxjs/add/observable/zip";
-import "rxjs/add/observable/interval";
-import "rxjs/add/operator/map";
-import "rxjs/add/operator/zip";
-import "rxjs/add/operator/filter";
-import "rxjs/add/operator/takeUntil";
-import "rxjs/add/operator/first";
-import "rxjs/add/operator/toPromise";
-import "rxjs/add/operator/debounce";
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
